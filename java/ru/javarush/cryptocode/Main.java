a1f748b087b9226dc2bbf5dca9c5a2f65afcaa74
vadimastashin <67231182+vadimastashin@users.noreply.github.com>
2022-03-28 21:59:37 +0300

    Created App Skeleton

--- java/ru/javarush/cryptocode/Main.java
+++ java/ru/javarush/cryptocode/Main.java
@@ -1,7 +1,46 @@
 package ru.javarush.cryptocode;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.BufferedWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Привет!");
+    public static void main(String[] args) throws Exception {
+        String message = "";
+        String encryptedMessage = "";
+        String decryptedMessage = "";
+        char key = 13;
+
+        // Читаем данные из файла
+        File file = new File("sample3.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        // Шифруем текст из файла
+        while ((message = br.readLine()) != null) {
+            String newMessage = message.toLowerCase();
+            for (int x = 0; x < newMessage.length(); x++) {
+                encryptedMessage += (char) (message.charAt(x) + key);
+            }
+        }
+
+        // Выводим зашифрованный текст на экран
+        System.out.println(encryptedMessage);
+
+        // Расшифровываем текст
+        for (int x = 0; x < encryptedMessage.length(); x++) {
+            decryptedMessage += (char) (encryptedMessage.charAt(x) - key);
+        }
+
+        // Записываем в файл
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+            writer.write(decryptedMessage);
+        } catch(IOException ex){
+            System.out.println("Не удалось записать данные в файл");
+        }
+
     }
 }
